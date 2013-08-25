72df2b3082a009cb1bed377e0a62f66b2c2a99d1
Dan Esparza <esparza.dan@gmail.com>
2013-08-25 11:56:04 -0400

    Initial check-in
    Includes command line parser library and initial solution and console
    app

---
+++ ATVEncodeTag/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATVEncodeTag
+{
+    /// <summary>
+    /// This assistant encodes and tags media (specifically TV shows) so that they
+    /// can be imported easily into iTunes.  This is meant to be part of a larger set
+    /// of utilities to automatically manage a media collection.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Parse commandline options
+
+
+        }
+    }
+}
