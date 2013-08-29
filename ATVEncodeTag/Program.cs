23d66b2033d4d4c75f2e58cf6b9df4889a114ee4
Dan Esparza <esparza.dan@gmail.com>
2013-08-28 20:24:53 -0400

    Added meta information and push notifications
    Added pushover app and user keys to config

--- ATVEncodeTag/Program.cs
+++ ATVEncodeTag/Program.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using ATVAssistant.Common;
+using PushoverClient;
 
 namespace ATVEncodeTag
 {
@@ -34,7 +35,17 @@ namespace ATVEncodeTag
             int atomicParsleyTimeout = Convert.ToInt32(ConfigurationManager.AppSettings["AtomicParsleyTimeout"]);
 
             //  Get after processing path:
-            string afterProcessingPath = ConfigurationManager.AppSettings["AfterProcessingPath"]; 
+            string afterProcessingPath = ConfigurationManager.AppSettings["AfterProcessingPath"];
+ 
+            //  Get artwork Path
+            string artworkBasePath = ConfigurationManager.AppSettings["ArtworkBasePath"];
+
+            //  Get show info filename
+            string showMetaInfoFile = ConfigurationManager.AppSettings["ShowMetaInfoFile"];
+
+            //  Pushover settings
+            string pushoverAppKey = ConfigurationManager.AppSettings["PushoverAppKey"];
+            string pushoverUserKey = ConfigurationManager.AppSettings["PushoverUserKey"];
 
             #endregion
 
@@ -49,8 +60,17 @@ namespace ATVEncodeTag
                 //  Next, parse our directory structure:
                 TVShowInfo showInfo = TVShowInfo.FromPathInfo(basePath, options.FileToProcess);
 
+                #region Get show meta information (artwork, ratings, etc)
+                
                 //  Figure out meta information location (artwork / ratings)
                 //  Get artwork and ratings for the show / season
+                string showInfoFullPath = Path.Combine(currentPath, showMetaInfoFile);
+                TVShowMetaInfoManager metaManager = new TVShowMetaInfoManager(showInfoFullPath, artworkBasePath);
+                TVShowMetaInfo metaShowInfo = metaManager.FindShowInfo(showInfo.Name, showInfo.SeasonNumber);
+
+                #endregion
+                
+                #region Encode with Handbrake
 
                 //  Determine output path for Handbrake 
                 string handbrakeOutput = Path.Combine(
@@ -58,8 +78,6 @@ namespace ATVEncodeTag
                     Path.GetFileNameWithoutExtension(options.FileToProcess) + ".m4v"
                     );
 
-                #region Encode with Handbrake
-                
                 //  Process in Handbrake and wait (using timeout)
                 ProcessStartInfo handbrakePInfo = new ProcessStartInfo();
 
@@ -86,15 +104,34 @@ namespace ATVEncodeTag
                 
                 //  Process in AtomicParsley and wait
                 ProcessStartInfo apPInfo = new ProcessStartInfo();
-                apPInfo.Arguments = string.Format(
-                    "\"{0}\" --genre \"TV Shows\" --stik \"TV Show\" --TVShowName \"{1}\" --TVEpisode \"{2}{3}\" --TVSeasonNum {2} --TVEpisodeNum {3} --artist \"{1}\" --title \"{4}\" --contentRating \"{5}\" --overWrite",
-                    handbrakeOutput,
-                    showInfo.Name,
-                    showInfo.SeasonNumber,
-                    showInfo.EpisodeNumber,
-                    showInfo.EpisodeTitle,
-                    "TV-14" /* Replace with lookup from local show database */
-                    /* Need to also add information on artwork from local show database */);
+
+                //  If we have ratings information & artwork
+                if(metaShowInfo != null)
+                {
+                    //  Use the ratings and artwork information
+                    apPInfo.Arguments = string.Format(
+                        "\"{0}\" --genre \"TV Shows\" --stik \"TV Show\" --TVShowName \"{1}\" --TVEpisode \"{2}{3}\" --TVSeasonNum {2} --TVEpisodeNum {3} --artist \"{1}\" --title \"{4}\" --contentRating \"{5}\" --artwork \"{6}\" --overWrite",
+                        handbrakeOutput,
+                        showInfo.Name,
+                        showInfo.SeasonNumber,
+                        showInfo.EpisodeNumber,
+                        showInfo.EpisodeTitle,
+                        metaShowInfo.Rating,
+                        metaShowInfo.ArtworkLocation
+                        );
+                }
+                else
+                {
+                    //  Otherwise, use simpler arguments
+                    apPInfo.Arguments = string.Format(
+                        "\"{0}\" --genre \"TV Shows\" --stik \"TV Show\" --TVShowName \"{1}\" --TVEpisode \"{2}{3}\" --TVSeasonNum {2} --TVEpisodeNum {3} --artist \"{1}\" --title \"{4}\" --overWrite",
+                        handbrakeOutput,
+                        showInfo.Name,
+                        showInfo.SeasonNumber,
+                        showInfo.EpisodeNumber,
+                        showInfo.EpisodeTitle
+                        );
+                }
 
                 apPInfo.FileName = Path.Combine(currentPath, "AtomicParsley.exe");
 
@@ -127,8 +164,24 @@ namespace ATVEncodeTag
 
                 #endregion
 
-                //  Send notification using Pushover
+                #region Send notification message
+                
+                if(!string.IsNullOrEmpty(pushoverAppKey) && !string.IsNullOrEmpty(pushoverUserKey))
+                {
+                    //  Create the push client
+                    Pushover pushClient = new Pushover(pushoverAppKey, pushoverUserKey);
 
+                    //  Format the message
+                    string message = string.Format(
+                        "Season {0} Episode {1} (\"{2}\") is ready to watch",
+                        showInfo.SeasonNumber,
+                        showInfo.EpisodeNumber,
+                        showInfo.EpisodeTitle);
+
+                    pushClient.Push(showInfo.Name, message);
+                } 
+
+                #endregion
             }
         }
     }
