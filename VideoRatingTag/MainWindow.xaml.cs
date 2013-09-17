1f578cd05c9fc743d610b395f54c1d0b67327b54
Dan Esparza <esparza.dan@gmail.com>
2013-09-17 08:16:23 -0400

    Added start of VideoRatingTag project

---
+++ VideoRatingTag/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoRatingTag
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ListBox_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void btnClearList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
